4548ed4cd994027a355da84099b36be0dbf2c2fc
hakGuenLee <alsfkr23@naver.com>
2024-10-06 15:58:51 +0900

    두번째 페이지 엑티비티 일부 수정

--- app/src/main/java/home/test/running/RunningStartActivity.kt
+++ app/src/main/java/home/test/running/RunningStartActivity.kt
@@ -1,17 +1,11 @@
 package home.test.running
 
+
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
-import android.widget.Toast
-import androidx.core.content.ContextCompat
 
-class RunningStartActivity : AppCompatActivity() {
+class RunningStartActivity : AppCompatActivity(){
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +15,10 @@ class RunningStartActivity : AppCompatActivity() {
 
 
     }
+
+
+
+
 
 
 
