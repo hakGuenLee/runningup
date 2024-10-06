f74e23dc2eacd781405a2f6da131a4c3bc505433
hakGuenLee <alsfkr23@naver.com>
2024-10-06 10:29:18 +0900

    권한 허용 요청 로직 추가(1차)

--- app/src/main/java/home/test/running/MainActivity.kt
+++ app/src/main/java/home/test/running/MainActivity.kt
@@ -5,8 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_PERMISSIONS = 1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        permissionRequestChecker()
+
     }
+
+    //권한 허용 요청
+    private fun permissionRequestChecker(){
+        var permissionList = mutableMapOf<String, String>()
+        permissionList["location"] = Manifest.permission.ACCESS_COARSE_LOCATION
+        permissionList["fineLocation"] = Manifest.permission.ACCESS_FINE_LOCATION
+        permissionList["audio"] = Manifest.permission.READ_MEDIA_AUDIO
+
+        //권한 허용 여부 확인
+        var permissionDeny = permissionList.count{ ContextCompat.checkSelfPermission(this,it.value) == PackageManager.PERMISSION_DENIED}
+
+        if(permissionDeny > 0 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            requestPermissions(permissionList.values.toTypedArray(),REQUEST_PERMISSIONS)
+        }
+
+
+    }
+
+
+
+
+
 }
